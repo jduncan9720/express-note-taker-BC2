de78e62d01e9f29224597de100f90e0dffbf490e
Joshua Duncan <joshuaduncan@Joshuas-MacBook-Pro.local>
2020-11-22 17:06:33 -0700

    fixing files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,11 +8,12 @@ module.exports = function(app){
     });
 
     app.post("/api/notes", function(req, res){
-        res.json(notesData)
+        notesData.push(req.body)
+        res.json(true)
     });
 
     app.delete("/api/notes/:id", function(req, res){
-        
+
     })
 
 }
